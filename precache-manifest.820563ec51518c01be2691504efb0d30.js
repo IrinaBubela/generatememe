55e3e1657574883d760ab41698b0f90bf48d7d2f
Irina <rishabubela@gmail.com>
2020-05-28 17:59:10 +0200

    Updates

---
+++ precache-manifest.820563ec51518c01be2691504efb0d30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21fd0e11e4a7be9f8e6f8aa3dbb3bb98",
+    "url": "/nazwa/index.html"
+  },
+  {
+    "revision": "4739b1372094f720e17f",
+    "url": "/nazwa/static/css/main.23a660ab.chunk.css"
+  },
+  {
+    "revision": "eed27119c2f53881e764",
+    "url": "/nazwa/static/js/2.8da10c2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nazwa/static/js/2.8da10c2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4739b1372094f720e17f",
+    "url": "/nazwa/static/js/main.611cd225.chunk.js"
+  },
+  {
+    "revision": "ed71d718c16b617043d2",
+    "url": "/nazwa/static/js/runtime-main.0b266d1c.js"
+  }
+]);
